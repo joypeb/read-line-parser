45c2c2ef2ffc77193009c8d03dbaba0e13704583
박은빈 <joypeb@naver.com>
2022-10-17 11:45:37 +0900

    db연동 select

--- src/main/java/com/dbexercise/UserDao2.java
+++ src/main/java/com/dbexercise/UserDao2.java
@@ -1,9 +1,6 @@
 package com.dbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Map;
 
 public class UserDao2 {
@@ -26,8 +23,28 @@ public class UserDao2 {
         conn.close();
     }
 
+    public void select() throws SQLException {
+        Map<String,String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+        Connection conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);
+        PreparedStatement ps = conn.prepareStatement(
+                "SELECT * from likelionDB.users where id = ?"
+        );
+
+        ps.setString(1, "12");
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        System.out.printf("id : %s, user : %s, password : %s",rs.getString(1),rs.getString(2),rs.getString(3));
+
+        ps.close();
+        conn.close();
+    }
+
     public static void main(String[] args) throws SQLException {
         UserDao2 userDao2 = new UserDao2();
-        userDao2.add();
+        //userDao2.add();
+        userDao2.select();
     }
 }
